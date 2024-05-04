870bdafb7e90d5c1d1b373f14e8017f99b4175db
ShaktiMan69 <jugalnaik14@gmail.com>
2024-05-05 01:41:50 +0530

    init

---
+++ static/assets/js/scripts.js
@@ -0,0 +1,84 @@
+/*!
+ * Start Bootstrap - Creative v7.0.7 (https://startbootstrap.com/theme/creative)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+ */
+//
+// Scripts
+//
+$(document).ready(function () {
+  //carousel options
+  $("#quote-carousel").carousel({
+    pause: true,
+    interval: 10000,
+  });
+  initializeTestimonialSwiper();
+});
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Navbar shrink function
+  var navbarShrink = function () {
+    const navbarCollapsible = document.body.querySelector("#mainNav");
+    // navbarCollapsible.classList.add('navbar-shrink')
+
+    if (!navbarCollapsible) {
+      return;
+    }
+    if (window.scrollY === 0) {
+      navbarCollapsible.classList.remove("navbar-shrink");
+    } else {
+      navbarCollapsible.classList.add("navbar-shrink");
+    }
+  };
+
+  // Shrink the navbar
+  navbarShrink();
+
+  // Shrink the navbar when page is scrolled
+  document.addEventListener("scroll", navbarShrink);
+
+  // Activate Bootstrap scrollspy on the main nav element
+  const mainNav = document.body.querySelector("#mainNav");
+  if (mainNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: "#mainNav",
+      rootMargin: "0px 0px -40%",
+    });
+  }
+
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector(".navbar-toggler");
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll("#navbarResponsive .nav-link")
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener("click", () => {
+      if (window.getComputedStyle(navbarToggler).display !== "none") {
+        navbarToggler.click();
+      }
+    });
+  });
+
+  // Activate SimpleLightbox plugin for portfolio items
+  new SimpleLightbox({
+    elements: "#portfolio a.portfolio-box",
+  });
+});
+
+const initializeTestimonialSwiper = () => {
+  new Swiper(".swiper", {
+    loop: true,
+    centeredSlides: true,
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    spaceBetween: 10,
+    slidesPerView: 2.5,
+    autoplay: {
+      delay: 2000,
+    },
+    speed: 1000,
+    // Navigation arrows
+  });
+};
