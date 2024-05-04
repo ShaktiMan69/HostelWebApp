5dd9136aa17c3e9c448188a74cd1efea7bc5e5b6
ShaktiMan69 <jugalnaik14@gmail.com>
2024-05-05 01:41:50 +0530

    init

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__, static_url_path='', static_folder='static/',)
 
+app = Flask(__name__, static_url_path='', static_folder='static/',)
+app.secret_key = 'super secret key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hosteldata.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -32,24 +33,21 @@ class Hostel(db.Model):
     def __repr__(self):
         return 'Hostel' + str(self.id)
 
-
 @app.route('/')
 def index():
     hostels = Hostel.query.all()
     return render_template("index.html", hostels=hostels)
 
-
 @app.route('/admin')
 def admin():
     info = HostelInfo.query.get(1)
-    return render_template("dashboard.html", info=info)
-
+    hostel_names = Hostel.query.with_entities(Hostel.hname).all()
+    return render_template("dashboard.html", info=info, hostel_names = hostel_names)
 
 @app.route('/hostels')
 def hostel():
     hostels = Hostel.query.all()
     return render_template('hostels.html', hostels=hostels)
-
 
 @app.route('/hostel-info/<int:id>',methods=['GET'])
 def abc(id):
@@ -66,7 +64,6 @@ def delete(id):
     db.session.delete(hostel)
     db.session.commit()
     return redirect('/hostels')
-
 
 @app.route('/add-hostel', methods=['GET', 'POST'])
 def addhostel():
@@ -89,7 +86,27 @@ def addhostel():
     else:
         return render_template('addhostel.html')
 
-
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        info = HostelInfo.query.get(1)
+        info.totalhostels = info.totalhostels + 1
+        info.boyshostels = info.boyshostels + 1
+        hname = request.form['hname']
+        warden = request.form['warden']
+        nrooms = request.form['nrooms']
+        nstudents = request.form['nstudents']
+        info.totalstudents += int(nstudents)
+        fee = request.form['fee']
+        messfee = request.form['messfee']
+        new_hostel = Hostel(hname=hname, warden=warden, nrooms=nrooms,
+                            nstudents=nstudents, fee=fee, messfee=messfee)
+        db.session.add(new_hostel)
+        db.session.commit()
+        return redirect('/admin')
+    else:
+        return render_template('register.html')
+    
 @app.route('/hostels/edit/<int:id>', methods=['GET', 'POST'])
 def update(id):
     info = Hostel.query.get_or_404(id)
@@ -108,7 +125,6 @@ def update(id):
     else:
         return render_template('edit-hostel.html', info=info)
 
-
 @app.route('/edit-info', methods=['GET', 'POST'])
 def edit():
     info = HostelInfo.query.get_or_404(1)
@@ -124,6 +140,7 @@ def edit():
     else:
         return render_template('edit.html', info=info)
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, port='8080')
