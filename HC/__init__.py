ee9ce0fb35caf00bf7efb9b7f90d37e65a679c72
ShaktiMan69 <jugalnaik14@gmail.com>
2024-05-28 15:43:13 +0530

    adibaba codebase

--- HC/__init__.py
+++ HC/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from pathlib import Path
+import os
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
+ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'} 
+UPLOAD_FOLDER = os.getcwd() + r'\uploads'  # Define the upload folder
+Path(UPLOAD_FOLDER).mkdir(parents=True, exist_ok=True)
+
 
 def create_app():
     app = Flask(__name__, static_url_path='', static_folder='static/',)
@@ -10,8 +17,21 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hosteldata.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
     db.init_app(app)
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import Warden
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        # since the user_id is just the primary key of our user table, use it in the query for the user
+        return Warden.query.get(int(user_id))
+    
     # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
@@ -21,3 +41,10 @@ def create_app():
     app.register_blueprint(main_blueprint)
 
     return app
+
+# Helper function to check allowed file extensions
+def allowed_file(filename):
+    return True# TODO
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
