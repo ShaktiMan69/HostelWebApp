93819dc8bc78a6b2cea2ee83a6fdf1d59cdb9e2b
ShaktiMan69 <jugalnaik14@gmail.com>
2024-06-09 03:48:58 +0530

    forgot to add apis fixed it

--- HC/__init__.py
+++ HC/__init__.py
@@ -58,6 +58,9 @@ def create_app():
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from .api import api as api_blueprint
+    app.register_blueprint(api_blueprint)
+
     return app
 
 # Helper function to check allowed file extensions
