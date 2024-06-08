ee974f580fe45b086a7fe22b7397ff7498973954
ShaktiMan69 <jugalnaik14@gmail.com>
2024-06-09 00:24:01 +0530

    trying tailwinds myself

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./HC/templates/*.html",
+      "./HC/static/assets/js/*.js"
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
