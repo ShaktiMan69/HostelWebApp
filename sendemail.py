f95e15c901ee5475ff603b5a819339c976e89f64
ShaktiMan69 <jugalnaik14@gmail.com>
2024-06-10 14:42:34 +0530

    removed password from github

--- sendemail.py
+++ sendemail.py
@@ -1,19 +1,23 @@
 #! /usr/local/bin/python
-SMTPserver = 'smtp.hostinger.com'
-sender =     'hostelcompanionapp@ezalts.shop'
 
-USERNAME = "hostelcompanionapp@ezalts.shop"
-PASSWORD = "***REMOVED***"
-
-import sys
-
+import os
 from smtplib import SMTP_SSL as SMTP       # this invokes the secure SMTP protocol (port 465, uses SSL)
 # from smtplib import SMTP                  # use this for standard SMTP protocol   (port 25, no encryption)
 
 # old version
 # from email.MIMEText import MIMEText
 from email.mime.text import MIMEText
+from dotenv import load_dotenv
 
+load_dotenv()
+
+SMTPserver = os.environ.get('SMTPserver')
+sender =  os.environ.get('sender')
+
+USER = os.environ.get('USER')
+PASSWORD = os.environ.get('PASSWORD')
+
+print(SMTPserver, sender, USER, PASSWORD)
 def send_email(destination, content='Congrats', subject='Your application for a Hostel Room has been submitted.'):
     try:
         msg = MIMEText(content, 'plain')
@@ -22,11 +26,14 @@ def send_email(destination, content='Congrats', subject='Your application for a 
 
         conn = SMTP(SMTPserver)
         conn.set_debuglevel(False)
-        conn.login(USERNAME, PASSWORD)
+        conn.login(USER, PASSWORD)
         try:
             conn.sendmail(sender, destination, msg.as_string())
         finally:
             conn.quit()
 
-    except:
-        sys.exit( "mail failed; %s" % "CUSTOM_ERROR" ) # give an error message
+    except Exception as e:
+        print("mail failed: ", e ) # give an error message
+
+if __name__ == '__main__':
+    send_email('***REMOVED***')
